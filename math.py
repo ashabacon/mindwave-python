fd24a60d080ac46e56f4a1451aea70e57c731d74
Abie Brauner <abrauner@ucsd.edu>
2020-03-07 13:59:49 -0800

    gui done but doesn't get real data from bci yet

--- math.py
+++ math.py
@@ -1,51 +1,130 @@
-#python code
+import threading
+import random
+from tkinter import *
+import time
+player_ans = ""
+correct_ans = 0
+score = 0
+time_remaining = 100
+count = 0
 
-stress = 0;
-score = 0;
-start = time.time()
-time_value = 100;
-while time.time() - start < time_value:
-    give_math_problems
+def gui():
+    print("gui running")
+    window = Tk()
 
-def give_math_problems:
-    type_math = rand(1,3): #4 if division
-    if type_math = 1:
-        operation = '+'
-    if type_math = 2:
-        operation = '-'
-    if type_math = 3:
-        operation = 'x'
-#    if type_math = 4:
-#        operation = '/'n
+    window.title("Math is Stress Free")
 
-    num_1 = rand (1,20);
-    num_2 = rand (1,10);
+    window.geometry('850x800')
 
-    print (num_1, operation, num_2);
-    (switch) #case depends on value of operation
-      case 1: ans = num_1 + num_2;
-      case 2: ans = num_1 - num_2;
-      case 3: ans = num_1 * num_3;
+    lbl = Label(window, text="2+1")
 
-    user_ans = input(num_1, operation, num_2, "=" :\n");
+    lbl.grid(column=0, row=0)
+    
+    score_lbl = Label(window, text="score: 0")
 
-    if user_ans == ans: #might be type issues here but I think python handles it correctly?
-      score += 1;
-    else:
-      score -= 1;
+    score_lbl.grid(column=2, row=3)
 
+    timer_lbl = Label(window, text="time: 100")
 
-#more updatey timer taken from stack overflow:
-#import signal
-#def timeout_handler(signal, frame):
-#    raise Exception('Time is up!')
-#signal.signal(signal.SIGALRM, timeout_handler)
-#signal.alarm.timeout(60) #60 is their value, ours would be 100
-#try:
-#    while lives > 0 # we dont have live so i think we would just put give_math_problems
-        # stuff
-#except:
-    # print score
+    timer_lbl.grid(column=2, row=6)
+        
+    stress_lbl = Label(window, text="score: 0")
 
-#https://www.google.com/search?rlz=1C5CHFA_enUS817US817&biw=1440&bih=766&ei=GQpUXr2vK5De-gTL2rKwBQ&q=python+timer+in+background+display&oq=python+timer+in+background+display&gs_l=psy-ab.3..33i22i29i30.956.2109..2268...0.2..0.101.671.7j1......0....1..gws-wiz.......0i71j0i22i30.G2fUARunSKE&ved=0ahUKEwj9n_u43ernAhUQr54KHUutDFYQ4dUDCAs&uact=5#kpvalbx=_rgtUXsjDBZjy-gT8mqXQBQ19
-#that video link might be helpful
+    stress_lbl.grid(column=6, row=6)
+    
+    stress_lbl.configure(text= "")
+    
+    txt = Entry(window,width=10)
+
+    txt.grid(column=1, row=0)
+    
+    txt.focus()
+    
+
+    def clicked():
+        global player_ans
+
+        player_ans = int(txt.get())
+        global score
+        
+        if player_ans == correct_ans:
+            
+            score += 1
+        else:
+            
+            score -= 1
+        disp_sc = "score" + str(score)
+        score_lbl.configure(text= disp_sc)
+        get_new_prob()
+        txt.delete(0, 'end')
+      
+        
+    def get_new_prob():
+        type_math = random.randint(1,3) #4 if division
+        global correct_ans  
+        num_1 = random.randint(1,20)
+        num_2 = random.randint(1,10)
+        if type_math == 1:
+            operation = '+'
+            correct_ans = num_1 + num_2
+        if type_math == 2:
+            operation = '-'
+            correct_ans = num_1 - num_2
+        if type_math == 3:
+            operation = 'x'
+            correct_ans = num_1 * num_2
+        
+#       if type_math = 4:
+#           operation = '/'n
+#           ans = num_1 / num_2
+
+        prob = str(num_1) + str(operation) + str(num_2)
+
+        lbl.configure(text= prob)
+    get_new_prob()
+
+    btn = Button(window, text="Submit", command=clicked)
+
+    btn.grid(column=2, row=0)
+    
+    timer_lbl.configure(text= "time left:" + str(time_remaining))
+    
+    def start_timer_in():
+        def timink():
+            print("timer_control running")
+            global time_remaining
+            
+            while time_remaining > 0:
+                global count
+                time.sleep(1)
+                time_remaining -= 1
+                timer_lbl.configure(text= "time left:" + str(time_remaining))
+                count += 1
+                print(count)
+                if count == 5:
+                    count = 0
+                    #get data from BCI
+                    #going to proxy it as a random number here
+                    med_measure = random.randint(1,100)
+                    if med_measure >= 75:
+                        #gain 2 sec
+                        time_remaining += 2
+                        stress_lbl.configure(text= "Un-Stressed! +2 seconds!")
+                    elif med_measure <= 25:
+                        #lose 2 sec
+                        time_remaining -= 2
+                        stress_lbl.configure(text= "Stressed! -2 seconds!")
+                    else:
+                        stress_lbl.configure(text= "Normal stress, no bonus or penalty")
+                elif count == 3:
+                    stress_lbl.configure(text= "")
+                    
+        
+        t = threading.Thread(target=timink)
+        t.start()
+        
+    
+    window.after(10, start_timer_in) 
+    window.mainloop()
+    
+gui()
